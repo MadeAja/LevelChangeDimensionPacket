6be03c14e3c408ecf597422c4f8bb35e7543883b
Clarence2810 <46648176+Clarence2810@users.noreply.github.com>
2020-12-22 22:59:16 +0800

    No more loading screen and movement stuck

--- src/Dapro718/LevelChangeDimensionScreen/LCDSMain.php
+++ src/Dapro718/LevelChangeDimensionScreen/LCDSMain.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Dapro718\LevelChangeDimensionScreen;
 
 use pocketmine\Player;
-use pocketmine\network\mcpe\protocol\{ChangeDimensionPacket, LevelChunkPacket};
+use pocketmine\network\mcpe\protocol\{ChangeDimensionPacket, PlayStatusPacket};
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityTeleportEvent;
@@ -28,13 +28,20 @@ final class LCDSMain extends PluginBase implements Listener{
             if($event->getTo()->getLevel()->getFolderName() !== $player->getLevel()->getFolderName()){
                 if(isset($this->levels[strtolower($event->getTo()->getLevel()->getFolderName())])){
                     if(!$player->hasPermission("levelchangedimensionscreen.noscreen")){
-                        $pk = new ChangeDimensionPacket();
-                        $pk->dimension = $this->levels[strtolower($event->getTo()->getLevel()->getFolderName())];
-                        $pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
-                        $pk->respawn = false;
-                        $player->sendDataPacket($pk);
-                        $pk = LevelChunkPacket::withoutCacheintval(intval($event->getTo()->getX()), intval($event->getTo()->getz()), 36, "");
-                        $player->sendDataPacket($pk);
+                        if($event->getFrom()->getLevel() === $event->getTo()->getLevel()) return; // If same world no need the loading screen
+						$pk = new ChangeDimensionPacket();
+						$pk->dimension = 1; // Nether first
+						$pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
+						$pk->respawn = false;
+						$player->sendDataPacket($pk);
+						$pk = new ChangeDimensionPacket();
+						$pk->dimension = 0; // Next Overworld
+						$pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
+						$pk->respawn = false;
+						$player->sendDataPacket($pk);
+						$pk = new PlayStatusPacket();
+						$pk->status = PlayStatusPacket::PLAYER_SPAWN; // No more loading screen stuck
+						$player->sendDataPacket($pk);
                     }
                 }
             }
