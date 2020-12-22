35fabb23bdcf274d858aa6e71ed2625374dac163
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-12-22 10:39:43 -0500

    Bugs, bugs, and more bugs :/

--- src/Dapro718/LevelChangeDimensionScreen/ClearScreenTask.php
+++ src/Dapro718/LevelChangeDimensionScreen/ClearScreenTask.php
@@ -6,6 +6,7 @@ namespace Dapro718\LevelChangeDimensionScreen;
 
 use pocketmine\Player;
 use pocketmine\level\Position;
+use pocketmine\math\Vector3;
 use pocketmine\scheduler\Task;
 use pocketmine\network\mcpe\protocol\{ChangeDimensionPacket, PlayStatusPacket, PlayerFogPacket};
 
@@ -13,21 +14,21 @@ class ClearScreenTask extends Task{
 
     private $player;
     
-    public function __construct(Player $player, Position $pos){
+    public function __construct(Player $player, Position $position){
         $this->player = $player;
     }
     
     public function onRun(int $tick): void{
         $pk = new ChangeDimensionPacket();
         $pk->dimension = 0;
-        $pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
+        $pk->position = new Vector3($this->position->getX(), $this->position->getY(), $this->position->getZ());
         $pk->respawn = false;
         $this->player->sendDataPacket($pk);
         $pk = new PlayStatusPacket();
         $pk->status = PlayStatusPacket::PLAYER_SPAWN;
         $this->player->sendDataPacket($pk);
         $pk = new PlayerFogPacket();
-        $pk->layers = [];
+        $pk->fogLayers = [];
         $this->player->sendDataPacket($pk);
     }
 }
