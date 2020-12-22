61c12871055eee269283e1f51d4bc839a39b3b80
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-12-22 10:20:11 -0500

    Clear screen after delay

--- src/Dapro718/LevelChangeDimensionScreen/LCDSMain.php
+++ src/Dapro718/LevelChangeDimensionScreen/LCDSMain.php
@@ -22,26 +22,20 @@ final class LCDSMain extends PluginBase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     
-    public function onLevelChange(EntityTeleportEvent $event): void{
+    public function onTeleport(EntityTeleportEvent $event): void{
         $player = $event->getEntity();
         if($player instanceof Player){
             if($event->getTo()->getLevel()->getFolderName() !== $player->getLevel()->getFolderName()){
                 if(isset($this->levels[strtolower($event->getTo()->getLevel()->getFolderName())])){
                     if(!$player->hasPermission("levelchangedimensionscreen.noscreen")){
-                        if($event->getFrom()->getLevel() === $event->getTo()->getLevel()) return; // If same world no need the loading screen
-						$pk = new ChangeDimensionPacket();
-						$pk->dimension = 1; // Nether first
-						$pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
-						$pk->respawn = false;
-						$player->sendDataPacket($pk);
-						$pk = new ChangeDimensionPacket();
-						$pk->dimension = 0; // Next Overworld
-						$pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
-						$pk->respawn = false;
-						$player->sendDataPacket($pk);
-						$pk = new PlayStatusPacket();
-						$pk->status = PlayStatusPacket::PLAYER_SPAWN; // No more loading screen stuck
-						$player->sendDataPacket($pk);
+                        if($event->getFrom()->getLevel() !== $event->getTo()->getLevel()){
+							$pk = new ChangeDimensionPacket();
+							$pk->dimension = $this->levels[strtolower($event->getTo()->getLevel()->getFolderName())];
+							$pk->position = new Vector3($event->getTo()->getX(), $event->getTo()->getY(), $event->getTo()->getZ());
+							$pk->respawn = false;
+							$player->sendDataPacket($pk);
+							$this->getScheduler()->scheduleDelayedTask(new ClearScreenTask($player), 60);
+						}
                     }
                 }
             }
